96605d0cb675d43799d25d43dcd86fca1d60a2aa
Dale Mitchell <dale@dbmitchell.net>
2026-02-08 12:58:36 +0800

    remove bundler test code from main

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,14 +1,10 @@
 import express from "express"
-import { InMemoryMediaFileRepository } from "./infrastructure/database/InMemoryMediaFileRepository.js"
 
 const PORT = 6969
 
 const app = express()
 
-const repo = new InMemoryMediaFileRepository()
-
 app.get("/", async (_, res) => {
-    await repo.findById("Hello")
     return res.send("Hello, world!")
 })
 
